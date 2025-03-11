19f985143b527c039f1cdb65505e260a8b79ace4
videssmart <vipmarketing@videssmart.com>
2025-03-10 19:44:30 -0600

    Actualización de clientes

--- pages/proyectos.php
+++ pages/proyectos.php
@@ -1,3 +1,43 @@
+<?php
+$proyectos = [
+    [
+        "nombreProyecto" => "Manejo de Redes",
+        "cliente" => "Mrcbusservice",
+        "responsable" => "Maria Dolores",
+        "fechaEntrega" => "00/00/0000",
+        "fechaPago" => "28",
+        "estado" => "Activo",
+        "pago" => "Q650.00"
+    ],
+    [
+        "nombreProyecto" => "Manejo de Redes - Personalizado",
+        "cliente" => "Gamaliel Hernandez",
+        "responsable" => "Maria Dolores",
+        "fechaEntrega" => "00/00/0000",
+        "fechaPago" => "14",
+        "estado" => "Activo",
+        "pago" => "Q325.00"
+    ],
+    [
+        "nombreProyecto" => "Manejo de Redes",
+        "cliente" => "Eswin Chilel",
+        "responsable" => "Maria Dolores",
+        "fechaEntrega" => "00/00/0000",
+        "fechaPago" => "14",
+        "estado" => "Activo",
+        "pago" => "Q472.50"
+    ],
+    [
+        "nombreProyecto" => "Manejo de Redes",
+        "cliente" => "Travel Click",
+        "responsable" => "Maria Dolores",
+        "fechaEntrega" => "00/00/0000",
+        "fechaPago" => "30",
+        "estado" => "Activo",
+        "pago" => "Q650"
+    ]
+]
+?>
 <div class="bannerProyectos">
     <h2>Gestión de Proyectos Videss Smart</h2>
     <p>Visualiza, administra y colabora en cada proyecto desde un solo lugar</p>
@@ -47,14 +87,18 @@
             <th>FECHA DE ENTREGA</th>
             <th>FECHA DE PAGO</th>
             <th>ESTADO</th>
+            <th>PAGO</th>
         </tr>
+        <?php foreach($proyectos as $proyecto): ?>
         <tr class="trBody">
-            <td data-th="col1">Manejo de Redes</td>
-            <td data-th="col2">Mrcbusservice</td>
-            <td data-th="col3">Maria Dolores</td>
-            <td data-th="col4">00/00/0000</td>
-            <td data-th="col5">28/02/2025</td>
-            <td data-th="col6" class="estado"><p>Activo</p></td>
+            <td data-th="col1"><?php echo $proyecto["nombreProyecto"]?></td>
+            <td data-th="col2"><?php echo $proyecto["cliente"]?></td>
+            <td data-th="col3"><?php echo $proyecto["responsable"]?></td>
+            <td data-th="col4"><?php echo $proyecto["fechaEntrega"]?></td>
+            <td data-th="col5"><?php echo $proyecto["fechaPago"]?></td>
+            <td data-th="col6" class="estado"><p><?php echo $proyecto["estado"]?></p></td>
+            <td data-th="col5"><?php echo $proyecto["pago"]?></td>
         </tr>
+        <?php endforeach;?>
     </table>
 </div>
