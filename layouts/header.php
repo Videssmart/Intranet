7bf3a2c814fdb690ae2d35b0549ed64215a23225
videssmart <vipmarketing@videssmart.com>
2025-04-20 21:53:52 -0600

    Actualizacion de la página de Flujo de Trabajo de Videss Smart

--- layouts/header.php
+++ layouts/header.php
@@ -11,6 +11,9 @@
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-rounded/css/uicons-regular-rounded.css'>
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-solid-straight/css/uicons-solid-straight.css'>
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-straight/css/uicons-regular-straight.css'>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-bold-rounded/css/uicons-bold-rounded.css'>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-solid-rounded/css/uicons-solid-rounded.css'>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-solid-rounded/css/uicons-solid-rounded.css'>
     <link rel="shortcut icon" href="assets/img/icon_white.png" type="image/png">
     <title>Intranet Smart</title>
 </head>
--- pages/flujo.php
+++ pages/flujo.php
@@ -1,21 +1,299 @@
-<div class="flujoDeTrabajo">
-    <h2>Flujo de Trabajo: La Ruta Hacia el Éxito</h2>
-    <p>Conoce el camino que seguimos para hacer que cada proyecto brille como una estrella.</p>
-    <a href="#mapa"><button>Explorar Flujo de Trabajo</button></a>
+<div class="bannerFlujo">
+    <h2><span class="iconBannerFlujo"><i class="fi fi-ss-bomb"></i></span> Flujo de Trabajo en Videss Smart</h2>
 </div>
 
-<div class="mapaFlujoTrabajo" id="mapa">
-    <h2 class="title" id="documentosApoyo"><span class="iconTitle"><img src="assets/img/flujoTrabajo.png" alt=""></span>Mapa de Flujo de Trabajo</h2>
-    <div class="servicios">
-        <div class="servicio">
-            <h3>Desarrollo Web</h3>
-        </div>
-        <div class="servicio">
-            <h3>Diseño Grafico</h3>
-        </div>
-        <div class="servicio">
-            <h3>Locución</h3>
-        </div>
+<div class="cuadroServicios">
+    <h3 class="titleDesign2">Cuadro General de Servicios</h3>
+    <div class="imagenServicios">
+        <img src="assets/img/areas.png" alt="">
     </div>
 </div>
 
+<div class="diagrama">
+    <h3 class="titleDesign2">Diagrama General de Flujo de Trabajo</h3>
+    <div class="diagramaCuadro">
+        <p>Este es un diagrama interactivo de flujo de trabajo completo. Pasa el cursor por encima para ver más detalles</p>
+        <div class="circulosTrabajo">
+            <ul class="listEtapas">
+                <li class="etapa e1">
+                    <h4>Atención</h4>
+                </li>
+                <li class="etapa e2">
+                    <h4>Cotización</h4>
+                </li>
+                <li class="etapa e3">
+                    <h4>Cierre</h4>
+                </li>
+                <li class="etapa e4">
+                    <h4>Desarrollo</h4>
+                </li>
+                <li class="etapa e5">
+                    <h4>Revisión</h4>
+                </li>
+                <li class="etapa e6">
+                    <h4>Entrega</h4>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+
+    <div class="resumenTrabajo">
+        <ul class="listResumen">
+            <li class="r1">
+                <p class="num">0</p>
+                <span class="iconResumen"><i class="fi fi-br-browser"></i></span>
+                <h5>Servicio Web</h5>
+            </li>
+            <li class="r2">
+                <p class="num">0</p>
+                <span class="iconResumen"><i class="fi fi-ss-pencil-paintbrush"></i></span>
+                <h5>Diseño Gráfico</h5>
+            </li>
+            <li class="r3">
+                <p class="num">0</p>
+                <span class="iconResumen"><i class="fi fi-sr-microphone-alt"></i></span>
+                <h5>Locución</h5>
+            </li>
+            <li class="r4">
+                <p class="num">0</p>
+                <span class="iconResumen"><i class="fi fi-sr-file-export"></i></span>
+                <h5>Entregas</h5>
+            </li>
+            <li class="r5">
+                <p class="num">0</p>
+                <span class="iconResumen"><i class="fi fi-ss-process"></i></span>
+                <h5>En Proceso</h5>
+            </li>
+        </ul>
+    </div>
+</div>
+
+
+<div class="protocolosFase">
+    <h3 class="titleDesign2">Protocolos por Fase</h3>
+    <p>Consulta los protocoloes específicos para cada fase del flujo de trabajo</p>
+
+    <div class="fases">
+        <div class="accordion accordion-flush" id="accordionFlushExample">
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                        <span class="icon"><i class="fi fi-ss-user-headset"></i></span> Atención al Cliente
+                    </button>
+                </h2>
+                <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                        <div class="info">
+                            <strong>¿Qué se hace?</strong><br>
+                            Recibir solicitudes, recopilar información del cliente, crear contacto inicial <br><br>
+                            <strong>¿Qué herramientas se usan?</strong><br>
+                            <div class="herramientas">
+                                <ul class="hr">
+                                    <li>HubSpot</li>
+                                    <li>Meta Suite Business</li>
+                                    <li>Intranet</li>
+                                    <li>Notion</li>
+                                </ul>
+                            </div>
+                            <br>
+                            <strong>¿Qué hacer si algo falla?</strong><br>
+                            <strong>Cliente no responde: </strong> Seguimiento telefónico después de 48h <br>
+                            <strong>Información incompleta: </strong> Usar checklist de información mínima requerida
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                        <span class="icon"><i class="fi fi-ss-document-signed"></i></span> Cotización
+                    </button>
+                </h2>
+                <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                        <div class="info">
+                            <strong>¿Qué se hace?</strong><br>
+                            Analizar requerimientos, calcular costos, preparar propuesta, negociar con el cliente <br><br>
+                            <strong>¿Qué herramientas se usan?</strong><br>
+                            <div class="herramientas">
+                                <ul class="hr">
+                                    <li>Documentos de Apoyo</li>
+                                    <li>Cotizador Web</li>
+                                    <li>Formularios de Cotización</li>
+                                </ul>
+                            </div>
+                            <br>
+                            <strong>¿Qué hacer si algo falla?</strong><br>
+                            <strong>Cliente solicita descuento: </strong> Consultar tabla de descuentos autorizados<br>
+                            <strong>Proyecto fuera de alcance: </strong> Escalar con el área para cotización especial
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                        <span class="icon"><i class="fi fi-sr-handshake"></i></span> Cierre
+                    </button>
+                </h2>
+                <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                    <div class="info">
+                            <strong>¿Qué se hace?</strong><br>
+                            Confirmar los últimos detallas del proyecto, validar entregables finales y acordar condiciones de pago y entrega<br><br>
+                            <strong>¿Qué herramientas se usan?</strong><br>
+                            <div class="herramientas">
+                                <ul class="hr">
+                                    <li>Documentos de Apoyo</li>
+                                    <li>Google Drive</li>
+                                    <li>Formulario de Conformidad</li>
+                                </ul>
+                            </div>
+                            <br>
+                            <strong>¿Qué hacer si algo falla?</strong><br>
+                            <strong>Cliente solicita modificación de ultimo momento: </strong> Evaluar si esta dentro del alcance inicial o si requiere costo adicional <br>
+                            <strong>Confunsión en la información brindada: </strong> Programar una breve reunión para aclarar y ajustar según sea necesario
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+                        <span class="icon"><i class="fi fi-sr-palette"></i></span> Diseño Gráfico
+                    </button>
+                </h2>
+                <div id="flush-collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                    <div class="info">
+                            <strong>¿Qué se hace?</strong><br>
+                            Crear conceptos visuales, diseñar interfaces, preparar artes gráficos <br><br>
+                            <strong>¿Qué herramientas se usan?</strong><br>
+                            <div class="herramientas">
+                                <ul class="hr">
+                                    <li>Paquetes Adobe</li>
+                                    <li>Canva</li>
+                                    <li>IA</li>
+                                </ul>
+                            </div>
+                            <br>
+                            <strong>¿Qué hacer si algo falla?</strong><br>
+                            <strong>Cliente rechaza propuesta: </strong> Reunión de feedback y nueva iteración<br>
+                            <strong>Falta de recursos: </strong> Consultar banco de recursos alternativos
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+                        <span class="icon"><i class="fi fi-ss-head-side-headphones"></i></span> Locución
+                    </button>
+                </h2>
+                <div id="flush-collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                    <div class="info">
+                            <strong>¿Qué se hace?</strong><br>
+                            Revisar guión, grabar voces, editar audio, aplicar efectos sonoros <br><br>
+                            <strong>¿Qué herramientas se usan?</strong><br>
+                            <div class="herramientas">
+                                <ul class="hr">
+                                    <li>Software de Edición</li>
+                                    <li>Estudio de Grabación</li>
+                                    <li>IA</li>
+                                </ul>
+                            </div>
+                            <br>
+                            <strong>¿Qué hacer si algo falla?</strong><br>
+                            <strong>Calidad de audio deficiente: </strong> Reprogramar sesión en estudio principal<br>
+                            <strong>Cambios en el guión: </strong> Evaluar impacto y cotizar adicionales si es necesario
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
+                        <span class="icon"><i class="fi fi-ss-site-browser"></i></span> Servicio Web
+                    </button>
+                </h2>
+                <div id="flush-collapseSix" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                    <div class="info">
+                            <strong>¿Qué se hace?</strong><br>
+                            Diseñar, desarrollar y mantener sitios web funcionales, seguros y optimizados <br><br>
+                            <strong>¿Qué herramientas se usan?</strong><br>
+                            <div class="herramientas">
+                                <ul class="hr">
+                                    <li>Lenguajes de Maquetado</li>
+                                    <li>Lenguajes de Programación</li>
+                                    <li>Hostinger</li>
+                                    <li>Git y GitHub</li>
+                                </ul>
+                            </div>
+                            <br>
+                            <strong>¿Qué hacer si algo falla?</strong><br>
+                            <strong>Error encontrado: </strong> Diagnóstico inmediato y corrección técnica<br>
+                            <strong>Cliente quiere cambios nuevos: </strong> Evaluar alcance y generar propuesta de mejora
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSeven" aria-expanded="false" aria-controls="flush-collapseSeven">
+                        <span class="icon"><i class="fi fi-ss-tools"></i></span> Desarrollo
+                    </button>
+                </h2>
+                <div id="flush-collapseSeven" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                    <div class="info">
+                            <strong>¿Qué se hace?</strong><br>
+                            Implementar solución técnica, integrar elementos visuales y de audio, realizar pruebas <br><br>
+                            <strong>¿Qué herramientas se usan?</strong><br>
+                            <div class="herramientas">
+                                <ul class="hr">
+                                    <li>IDEs</li>
+                                    <li>Frameworks</li>
+                                    <li>Control de Versiones</li>
+                                </ul>
+                            </div>
+                            <br>
+                            <strong>¿Qué hacer si algo falla?</strong><br>
+                            <strong>Bugs criticos: </strong> Priorizar corrección y notificar al clien te sobre retrasos<br>
+                            <strong>Incompatibilidad técnica: </strong> Documentar y proponer alternativas viables
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseEight" aria-expanded="false" aria-controls="flush-collapseEight">
+                        <span class="icon"><i class="fi fi-ss-shipping-fast"></i></span> Entrega
+                    </button>
+                </h2>
+                <div id="flush-collapseEight" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                    <div class="info">
+                            <strong>¿Qué se hace?</strong><br>
+                            Revisión final, preparación de documentación, entrega al cliente, seguimiento post-entrega <br><br>
+                            <strong>¿Qué herramientas se usan?</strong><br>
+                            <div class="herramientas">
+                                <ul class="hr">
+                                    <li>Google Drive</li>
+                                    <li>Encuesta de Satisfacción</li>
+                                    <li>HubSpot</li>
+                                </ul>
+                            </div>
+                            <br>
+                            <strong>¿Qué hacer si algo falla?</strong><br>
+                            <strong>Cliente solicita cambios adicionales: </strong> Evaluar alcance y cotizar como mantenimiento <br>
+                            <strong>Problemas en producción: </strong> Activar protocolo de soporte urgente
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
